4562b3f497f67b190fdb6eb95e9e6db72e586019
Jonas Bernoulli <jonas@bernoul.li>
2016-10-08 22:58:08 +0200

    use https in homepage link

--- morlock.el
+++ morlock.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2013-2016  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
-;; Homepage: http://github.com/tarsius/morlock
+;; Homepage: https://github.com/tarsius/morlock
 ;; Keywords: convenience
 
 ;; This file is not part of GNU Emacs.
