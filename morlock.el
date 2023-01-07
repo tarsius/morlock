7853149fe297399f2b6c1f58c4eebfb46b45510e
Jonas Bernoulli <jonas@bernoul.li>
2023-01-07 16:19:02 +0100

    Bump copyright years

--- morlock.el
+++ morlock.el
@@ -1,6 +1,6 @@
 ;;; morlock.el --- More font-lock keywords for elisp  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2013-2022 Jonas Bernoulli
+;; Copyright (C) 2013-2023 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/morlock
