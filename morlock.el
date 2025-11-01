02759c4d05ef6ec6b05707dab5bfe9a148a2c5ac
Jonas Bernoulli <jonas@bernoul.li>
2025-11-01 21:37:16 +0100

    Release version 2.1.2

--- morlock.el
+++ morlock.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/morlock
 ;; Keywords: convenience
 
-;; Package-Version: 2.1.1
+;; Package-Version: 2.1.2
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
